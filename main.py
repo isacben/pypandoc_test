f2c118b3502e2cdf478f51b2a5c1d908a2be2358
Isaac Benitez <ibsandoval@gmail.com>
2023-08-11 15:32:41 -0700

    add header

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
 import pypandoc
 
 def main():
-    output = pypandoc.convert_file('test.md', 'pdf', outputfile='test.pdf')
+    # https://stackoverflow.com/questions/40988240/pandoc-latex-pdf-headed-and-footer
+    args = [
+        '-H',
+        'header.tex',
+        '-V',
+        'geometry:margin=2.5cm'
+    ]
+
+    output = pypandoc.convert_file(
+        'test.md',
+        'pdf',
+        outputfile='test.pdf',
+        extra_args=args
+    )
 
 if __name__ == "__main__":
     main()
