142d1da8ada0a88634504b42ac752ba3092a28e6
Isaac Benitez <ibsandoval@gmail.com>
2023-08-10 22:20:14 -0700

    remove comments

--- main.py
+++ main.py
@@ -2,10 +2,6 @@ import pypandoc
 
 def main():
     output = pypandoc.convert_file('test.md', 'pdf', outputfile='test.pdf')
-    # print(type(output))
-
-    # f = open("test.pdf", "x")
-    # f.write
 
 if __name__ == "__main__":
     main()
